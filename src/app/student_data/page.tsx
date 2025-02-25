ee4c1ce26dba02a90955afbbb6465be331a6c5bf
AdrianaRoza <adriana.roza.aka@gmail.com>
2025-02-25 08:49:40 -0400

    refactor

--- src/app/student_data/page.tsx
+++ src/app/student_data/page.tsx
@@ -1,32 +1,9 @@
 'use client';
 
-import { useStudent } from "../context/studentcontext";
 
 const Student_data = () =>{
-    const {selectedStudent} = useStudent();
-
-    if (!selectedStudent) {
-        return <p className="text-red-500">Nenhum Aluno Selecionado</p>
-    }
-
     return (
-        <div className="text-black p-4">
-            <h2 className="text-xl font-bold">Dados do Aluno</h2>
-            <p>
-              <strong>Nome:</strong>
-							{selectedStudent.name} 
-            </p>
-
-						<p>
-							<strong>Usuário:</strong>
-							{selectedStudent.username}
-						</p>
-
-						<p>
-							<strong>Email:</strong>
-							{selectedStudent.email}
-						</p>
-        </div>
+        <div className="text-black">Dados do Estudante</div>
     )
 }
 
--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,31 +1,11 @@
 'use client';
 
-import { useStudent } from "../context/studentcontext";
-import Link from "next/link";
 
 const Students = () => {
-    const { students, selectStudent } = useStudent();
 
     return (
-        <div className="p-6">
-            <h1 className="text-xl font-bold">Alunos</h1>
-            <div className="mt-4">
-                {students.map((student) => (
-                    <div key={student.id} className="mb-4">
-                        <Link
-                            href={`/student_data`} // Link para a página do estudante
-                            className="block p-4 bg-gray-200 rounded hover:bg-gray-300"
-                            onClick={() => selectStudent(student.id)} // Seleciona o aluno
-                        >
-                            <h2 className="text-lg font-semibold">{student.name}</h2>
-                            <p className="text-gray-600">@{student.username}</p>
-                            <p className="text-sm text-gray-500">{student.email}</p>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-};
+        <div className="text-black p-6">Alunos</div>
+    )
+}
 
 export default Students;
